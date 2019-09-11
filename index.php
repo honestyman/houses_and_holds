0b97bcf7c18032a22d26c886a45d632210114cf8
honestyman <leel50296@gmail.com>
2019-09-10 22:35:24 -0500

    Add chatbox (partial)

--- index.php
+++ index.php
@@ -5,6 +5,18 @@
 <title>Houses and Holds</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style>
+#chatbox {
+    text-align:left;
+    margin-bottom:10px;
+    margin-top:10px;
+    padding:10px;
+    background:#fff;
+    height:270px;
+    width:450px;
+    border:1px solid #000;
+    overflow:auto; }
+</style>
 </head>
 <body>
 <div>
@@ -13,21 +25,63 @@
 
 require_once "src/charMakeOffline.php";
 
+session_start();
+
 // Login/signup form
-echo "<form action='dashboard.php' method='post'>";
-echo "<p><input type='email' name='username' placeholder='Email' size='40' /></p>";
+function loginForm(){
+echo "<form action='index.php' method='post'>";
+echo "<p><input type='email' name='user_email' placeholder='Email' size='40' /></p>";
 echo "<p><input type='password' name='password' placeholder='Password' size='40' /></p>";
 echo "<p><input type='password' name='password2' placeholder='Re-type password to join (new user)' size='40' /></p>";
 echo "<p><input type='submit' name='login' value='Log in / Sign up' /></p>";
 echo "</form>";
+}
+
+// Check if login form is submitted
+if(isset($_POST['login'])){
+  if($_POST['user_email'] != ""){
+    $_SESSION['user_email'] = stripslashes(htmlspecialchars($_POST['user_email']));
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>" . $_SESSION['user_email'] . " has logged in.<br /></div>");
+    fclose($fp);
+  } else {
+    echo "<span class='error'>Please type in an email.</span>";
+  }
+}
+
+if(!isset($_SESSION['user_email'])){
+  loginForm();
+} else {
+  echo "<p>Logged in as " . $_SESSION['user_email'];
+  echo "<form action='index.php' method='post'>";
+  echo "<input type='hidden' name='exit_email' value='" . $_SESSION['user_email'] . "' />";
+  echo "<input type='submit' name='logout' value='Logout' />";
+  echo "</form></p>";
+
+  echo "<div id='chatbox'>";
+  if(file_exists("log.html") && filesize("log.html") > 0){
+    $handle = fopen("log.html", "r");
+    $contents = fread($handle, filesize("log.html"));
+    fclose($handle);
+
+    echo $contents;
+  }
+  echo "</div>";
+  echo "<form name='message' action=''>";
+  echo "<input name='usermsg' type='text' id='usermsg' size='63' />";
+  echo "<input name='submitmsg' type='submit' id='submitmsg' value='Send' />";
+  echo "</form>";
+}
 
 // Check if the logout form is submitted
 if ( isset( $_POST['logout'] ) ) {
-  $user_email = "";
-  $pw = "";
-  $pw2 = "";
-  echo "<p>You are logged out</p>";
-  exit;
+  $exit_email = $_POST['exit_email'];
+  $fp = fopen("log.html", 'a');
+  fwrite($fp, "<div class='msgln'>" . $exit_email . " has logged out.<br /></div>");
+  fclose($fp);
+
+  session_destroy();
+  header("Location: index.php");
 }
 
 // Check if quitlogout form is submitted
@@ -51,6 +105,52 @@ if ( isset( $_POST['logoutfail'] ) ) {
 }
 
 ?>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+});
+
+//jQuery Document
+$(document).ready(function(){
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}
+	});
+});
+
+//If user submits the form
+$("#submitmsg").click(function(){
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});
+		$("#usermsg").attr("value", "");
+		loadLog();
+	return false;
+});
+
+function loadLog(){
+	var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+	$.ajax({
+		url: "log.html",
+		cache: false,
+		success: function(html){
+			$("#chatbox").html(html); //Insert chat log into the #chatbox div
+
+			//Auto-scroll
+			var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+			if(newscrollHeight > oldscrollHeight){
+				$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+			}
+	  	},
+	});
+}
+
+setInterval (loadLog, 2500);
+</script>
+
 </div>
 </body>
 </html>
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <html><body>
 <?php
-echo password_hash("", PASSWORD_DEFAULT);
+echo password_hash("1234", PASSWORD_DEFAULT);
  ?>
 </body></html>
