5bb734b477cbe17ef5624f128b52051c3fb0896c
honestyman <leel50296@gmail.com>
2019-09-18 21:57:42 -0500

    Add age check to pledge review

--- src/reviewPledges.php
+++ src/reviewPledges.php
@@ -1,4 +1,6 @@
 <?php
+require_once "characterAge.php";
+
 function reviewPledges($connect, $char_id, $user_email, $user_id)
 {
   $sqls = "SELECT * FROM characters WHERE id=" . $char_id;
@@ -7,71 +9,83 @@ function reviewPledges($connect, $char_id, $user_email, $user_id)
   if($ress->num_rows == 1)
   {
     $char = $ress->fetch_assoc();
+    $age = characterAge($char['birth_date']);
 
-    if(is_null($char['house_id']))
+    if($age == 'child' || $age == 'youth')
     {
-      echo "<p>You don't belong to a House.</p>";
+      echo "<p>Children and youths cannot review pledges.</p>";
     }
-    else
+    else if($age == 'adult' || $age == 'elder')
     {
-      $sqlp = "SELECT * FROM pledges WHERE house_id=" . $char['house_id'];
-      $resp = mysqli_query($connect, $sqlp);
-
-      if($resp->num_rows > 0)
+      if(is_null($char['house_id']))
       {
-        // display pledges as a table
-        echo "<table><tr><th>Pledges</th></tr>";
-        while($pledge = $resp->fetch_assoc())
-        {
-          // one line
-          $sqlc = "SELECT * FROM characters WHERE id=" . $pledge['char_id'];
-          $resc = mysqli_query($connect, $sqlc);
-
-          if($resc->num_rows == 1)
-          {
-            $pc = $resc->fetch_assoc();
-
-            echo "<tr><td>";
-            echo $pc['name'];
-            echo "</td><td>";
-
-            // Check status, add button
-            if($pledge['accepted']==1)
-            {
-              echo "Accepted";
-            }
-            else if($pledge['rejected']==1)
-            {
-              echo "Rejected";
-            }
-            else
-            {
-              echo "<form action='interaction.php' method='post'>";
-              echo "<select name='decision'>";
-              echo "<option value='accept'>Accept</option>";
-              echo "<option value='reject'>Reject</option>";
-              echo "</select>";
-              echo "<input type='hidden' name='user_email' value='" . $user_email . "' />";
-              echo "<input type='hidden' name='user_id' value='" . $user_id . "' />";
-              echo "<input type='hidden' name='char_id' value='" . $char_id . "' />";
-              echo "<input type='hidden' name='pledge_id' value='" . $pledge['id'] . "' />";
-              echo "<input type='submit' name='decide_pledge' value='Do' />";
-              echo "</form>";
-            }
-
-            echo "</td></tr>";
-          }
-          else
-          {
-            echo "<p>Oh, this is broken isn't it?</p>";
-          }
-        }
-        echo "</table>";
+        echo "<p>You don't belong to a House.</p>";
       }
       else
       {
-        echo "<p>Your House has no pledges.</p>";
+        $sqlp = "SELECT * FROM pledges WHERE house_id=" . $char['house_id'];
+        $resp = mysqli_query($connect, $sqlp);
+
+        if($resp->num_rows > 0)
+        {
+          // display pledges as a table
+          echo "<table><tr><th>Pledges</th></tr>";
+          while($pledge = $resp->fetch_assoc())
+          {
+            // one line
+            $sqlc = "SELECT * FROM characters WHERE id=" . $pledge['char_id'];
+            $resc = mysqli_query($connect, $sqlc);
+
+            if($resc->num_rows == 1)
+            {
+              $pc = $resc->fetch_assoc();
+
+              echo "<tr><td>";
+              echo $pc['name'];
+              echo "</td><td>";
+
+              // Check status, add button
+              if($pledge['accepted']==1)
+              {
+                echo "Accepted";
+              }
+              else if($pledge['rejected']==1)
+              {
+                echo "Rejected";
+              }
+              else
+              {
+                echo "<form action='interaction.php' method='post'>";
+                echo "<select name='decision'>";
+                echo "<option value='accept'>Accept</option>";
+                echo "<option value='reject'>Reject</option>";
+                echo "</select>";
+                echo "<input type='hidden' name='user_email' value='" . $user_email . "' />";
+                echo "<input type='hidden' name='user_id' value='" . $user_id . "' />";
+                echo "<input type='hidden' name='char_id' value='" . $char_id . "' />";
+                echo "<input type='hidden' name='pledge_id' value='" . $pledge['id'] . "' />";
+                echo "<input type='submit' name='decide_pledge' value='Do' />";
+                echo "</form>";
+              }
+
+              echo "</td></tr>";
+            }
+            else
+            {
+              echo "<p>Oh, this is broken isn't it?</p>";
+            }
+          }
+          echo "</table>";
+        }
+        else
+        {
+          echo "<p>Your House has no pledges.</p>";
+        }
       }
+    }
+    else
+    {
+      echo "<p>Oh shit.</p>";
     }
   }
   else
