7aa1615aa8e87b8ebd77b22655bfd36c33480fcc
honestyman <leel50296@gmail.com>
2019-09-18 22:04:43 -0500

    Add age check to House registry

--- src/registerHouse.php
+++ src/registerHouse.php
@@ -1,11 +1,40 @@
 <?php
+require_once "characterAge.php";
+
 function registerHouse($connect, $char_id, $user_email, $user_id)
 {
-  echo "<form action='interaction.php' method='post'>";
-  echo "<input type='hidden' name='user_email' value='" . $user_email . "' />";
-  echo "<input type='hidden' name='user_id' value='" . $user_id . "' />";
-  echo "<input type='hidden' name='char_id' value='" . $char_id . "' />";
-  echo "<p><input type='text' name='house_name' placeholder='Enter name for House' size='40' /></p>";
-  echo "<p><input type='submit' name='house_register' value='Submit' /></p>";
+  // Chech character's age
+  $sqls = "SELECT * FROM characters WHERE id=" . $char_id;
+  $ress = mysqli_query($connect, $sqls);
+
+  if($ress->num_rows == 1)
+  {
+    $char = $ress->fetch_assoc();
+    $age = characterAge($char['birth_date']);
+
+    if($age == 'child' || $age == 'youth')
+    {
+      echo "<p>Children and youths cannot register a House.</p>";
+    }
+    else if($age == 'adult' || $age == 'elder')
+    {
+      echo "<form action='interaction.php' method='post'>";
+      echo "<input type='hidden' name='user_email' value='" . $user_email . "' />";
+      echo "<input type='hidden' name='user_id' value='" . $user_id . "' />";
+      echo "<input type='hidden' name='char_id' value='" . $char_id . "' />";
+      echo "<p><input type='text' name='house_name' placeholder='Enter name for House' size='40' /></p>";
+      echo "<p><input type='submit' name='house_register' value='Submit' /></p>";
+    }
+    else
+    {
+      echo "<p>Uh oh!</p>";
+    }
+
+  }
+  else
+  {
+    echo "<p>Uh oh!</p>";
+  }
+
 }
  ?>
