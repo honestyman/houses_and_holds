d774f613e05ea32a7cba7e6898a360233f736944
honestyman <leel50296@gmail.com>
2019-09-21 12:16:45 -0500

    Add check cooldown for aggression on char disconnect

--- src/charMakeOffline.php
+++ src/charMakeOffline.php
@@ -1,18 +1,52 @@
 <?php
-function charMakeOffline($connect, $char_id){
-  $sql = "UPDATE characters SET is_online=0 WHERE id=" . $char_id;
-  $result = mysqli_query($connect, $sql);
+function charMakeOffline($connect, $char_id)
+{
+  // check last aggression first
+  $sqls = "SELECT * FROM characters WHERE id=" . $char_id;
+  $ress = mysqli_query($connect, $sqls);
 
-  $sql = "SELECT * FROM characters WHERE id=" . $char_id;
-  $result = mysqli_query($connect, $sql);
-  if($result->num_rows>0)
+  if($ress->num_rows == 1)
   {
-    while($row = $result->fetch_assoc())
+    $char = $ress->fetch_assoc();
+
+    if(!is_null($char['last_aggression']))
     {
-      // Info to pass to game
-      $char = $row;
+      $now = new DateTime('NOW');
+      $now = $now->format('Y-m-d H:i:s');
+      $now = strtotime($now);
+      $last_aggression = strtotime($char['last_aggression']);
+      $aggression_interval = $now-$last_aggression;
+    }
+
+    if(is_null($char['last_aggression']) || $aggression_interval > $char['travel_cooldown'])
+    {
+      $sql = "UPDATE characters SET is_online=0 WHERE id=" . $char_id;
+      $result = mysqli_query($connect, $sql);
+
+      $sql = "SELECT * FROM characters WHERE id=" . $char_id;
+      $result = mysqli_query($connect, $sql);
+      
+      if($result->num_rows == 1)
+      {
+        $char = $result->fetch_assoc();
+      }
+      else
+      {
+        echo "<p>Who are you?</p>";
+      }
+    }
+    else
+    {
+      echo "<p>You can't do that yet.</p>";
     }
   }
+  else
+  {
+    echo "<p>Who are you?</p>";
+  }
+
+
+
 
   return $char;
 }
