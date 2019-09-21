c1513c6b10fb7a41d3de22f9242d07a27f53504a
honestyman <leel50296@gmail.com>
2019-09-21 11:59:33 -0500

    Add check cooldown for aggression on banner capture

--- src/captureBanner.php
+++ src/captureBanner.php
@@ -48,52 +48,69 @@ function captureBanner($connect, $banner_id, $char_id)
         echo "<p>Your House already controls this banner.</p>";
       }
       else {
-        // Check character's age first
-
-        // Capture banner
-        $sqlcap = "UPDATE banner_posts SET house_id=" . $rowc['house_id'] . " WHERE id=" . $banner_id;
-        mysqli_query($connect, $sqlcap);
-        echo "<p>Banner post captured!</p>";
-
-        // Check other Hold banners
-        $sqlloc = "SELECT * FROM locations WHERE id=" . $rowb['location_id'];
-        $resloc = mysqli_query($connect, $sqlloc);
-
-        if($resloc->num_rows==1)
+        // Check character's last aggression
+        if(!is_null($rowc['last_aggression']))
         {
-          $rowloc = $resloc->fetch_assoc();
+          $now = new DateTime('NOW');
+          $now = $now->format('Y-m-d H:i:s');
+          $now = strtotime($now);
+          $last_aggression = strtotime($rowsc['last_aggression']);
+          $aggression_interval = $now-$last_aggression;
+        }
+
+        if(is_null($rowc['last_aggression']) || $aggression_interval > $rowc['aggression_cooldown'])
+        {
+          // Capture banner
+          $sqlcap = "UPDATE banner_posts SET house_id=" . $rowc['house_id'] . " WHERE id=" . $banner_id;
+          mysqli_query($connect, $sqlcap);
+          $sqlu = "UPDATE characters SET last_aggression=CURRENT_TIMESTAMP WHERE id=" . $char_id;
+          mysqli_query($connect, $sqlu);
+          echo "<p>Banner post captured!</p>";
+
+          // Check other Hold banners
+          $sqlloc = "SELECT * FROM locations WHERE id=" . $rowb['location_id'];
+          $resloc = mysqli_query($connect, $sqlloc);
+
+          if($resloc->num_rows==1)
+          {
+            $rowloc = $resloc->fetch_assoc();
+          }
+          else
+          {
+            echo "<p>Bad game! No cookie for you!</p>";
+          }
+
+          $sqlban = "SELECT banner_posts.id AS id, banner_posts.house_id AS house_id FROM banner_posts LEFT JOIN locations ON banner_posts.location_id = locations.id WHERE locations.hold_id=" . $rowloc['hold_id'];
+          $resban = mysqli_query($connect, $sqlban);
+
+          if($resban->num_rows>0)
+          {
+            $capture = 1;
+
+            while($rowban = $resban->fetch_assoc())
+            {
+              //echo $rowban['house_id'] . "<br />";
+              if(!is_null($rowban['id']))
+              {
+                if(is_null($rowban['house_id'])){$capture=0;}
+                if($rowban['house_id']!=$rowc['house_id']){$capture=0;}
+              }
+
+            }
+
+            //echo $capture;
+
+            if($capture==1)
+            {
+              $sqlhold = "UPDATE holds SET house_id=" . $rowc['house_id'] . " WHERE id=" . $rowloc['hold_id'];
+              mysqli_query($connect, $sqlhold);
+              echo "<p>Hold captured!</p>";
+            }
+          }
         }
         else
         {
-          echo "<p>Bad game! No cookie for you!</p>";
-        }
-
-        $sqlban = "SELECT banner_posts.id AS id, banner_posts.house_id AS house_id FROM banner_posts LEFT JOIN locations ON banner_posts.location_id = locations.id WHERE locations.hold_id=" . $rowloc['hold_id'];
-        $resban = mysqli_query($connect, $sqlban);
-
-        if($resban->num_rows>0)
-        {
-          $capture = 1;
-
-          while($rowban = $resban->fetch_assoc())
-          {
-            //echo $rowban['house_id'] . "<br />";
-            if(!is_null($rowban['id']))
-            {
-              if(is_null($rowban['house_id'])){$capture=0;}
-              if($rowban['house_id']!=$rowc['house_id']){$capture=0;}
-            }
-
-          }
-
-          //echo $capture;
-
-          if($capture==1)
-          {
-            $sqlhold = "UPDATE holds SET house_id=" . $rowc['house_id'] . " WHERE id=" . $rowloc['hold_id'];
-            mysqli_query($connect, $sqlhold);
-            echo "<p>Hold captured!</p>";
-          }
+          echo "<p>It's too soon for you to do that.</p>";
         }
       }
     }
