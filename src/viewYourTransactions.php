395204a2ec88dbcfbe69bba3b37709e8441ceb8a
honestyman <leel50296@gmail.com>
2019-09-22 23:39:28 -0500

    Add interaction dropdown to transactions

--- src/viewYourTransactions.php
+++ src/viewYourTransactions.php
@@ -17,6 +17,32 @@ function viewYourTransactions($connect, $char_id, $user_email, $user_id)
       echo $transo['created'];
       echo "</td><td>";
       echo displayTransactionItems($connect, $transo['items']);
+
+      // Add interaction option, if available
+      $class_id = 10; // All own transactions have class id 10
+      $sqli = "SELECT * FROM class_interactions WHERE class_id=" . $class_id;
+      $resulti = mysqli_query($connect, $sqli);
+
+      if($resulti->num_rows > 0)
+      {
+        echo "</td><td>";
+        echo "<form action='interaction.php' method='post'>";
+        echo "<select name='interaction'>";
+
+        while($rowi = $resulti->fetch_assoc())
+        {
+          echo "<option value='" . $rowi['name'] . "'>" . $rowi['displayname'] . "</option>";
+        }
+
+        echo "</select>";
+        echo "<input type='hidden' name='obj_id' value='" . $row1['id'] . "' />";
+        echo "<input type='hidden' name='user_email' value='" . $user_email . "' />";
+        echo "<input type='hidden' name='user_id' value='" . $user_id . "' />";
+        echo "<input type='hidden' name='char_id' value='" . $char_id . "' />";
+        echo "<input type='submit' name='interact' value='Do' />";
+        echo "</form>";
+      }
+
       echo "</td></tr>";
     }
 
